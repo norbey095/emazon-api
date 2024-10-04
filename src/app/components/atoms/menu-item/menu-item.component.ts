ea6267178467786043f1f79119a7486e6d204678
norbey095 <norbey095@gmail.com>
2024-10-04 13:47:14 -0500

    Se sube avance del menú donde va a estar las categorias listadas

--- src/app/components/atoms/menu-item/menu-item.component.ts
+++ src/app/components/atoms/menu-item/menu-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-item',
+  selector: 'menu-item',
   templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.scss'],
   encapsulation: ViewEncapsulation.None
--- src/app/components/molescules/navbar/navbar.component.ts
+++ src/app/components/molescules/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,9 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class NavbarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
