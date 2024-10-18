2cb3db9ddd71a6d7aa0581c7cae1d8a4d1e12017
norbey095 <norbey095@gmail.com>
2024-10-18 11:56:23 -0500

    Se realizo un ajuste a los input para cuando se exceda el limite de caracteres muestre el mensaje.

--- src/app/design-system/molecules/components/basic-form/basic-form.component.ts
+++ src/app/design-system/molecules/components/basic-form/basic-form.component.ts
@@ -8,12 +8,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./basic-form.component.scss']
 })
 export class BasicFormComponent {
+  messagesName: string = '';
+  messagesDescription: string = '';
+  lengthDescriptionError: string = '';
+  lengthNameError: string = '';
   categoryName: string = '';
   categoryDescription: string = '';
+  
   constructor(private router: Router) {}
-  @Input() urlBack: string= '';
 
-  @Output() formSubmit = new EventEmitter<{ name: string, description: string }>();
+  @Input() urlBack: string = '';
+  @Output() formSubmit = new EventEmitter<{ name: string, description: string }>();  
 
   onSubmit(form: NgForm) {
     if (form.valid) {
@@ -22,12 +27,30 @@ export class BasicFormComponent {
       this.categoryDescription = '';
       form.resetForm();
     } else {
+      this.messagesName = "El nombre es requerido.";
+      this.messagesDescription = "La descripción es requerida.";
       form.controls['name'].markAsTouched();
-      form.controls['description'].markAsTouched();
+      form.controls['description'].markAsTouched();      
     }
   }
 
   goBack() {
     this.router.navigate([this.urlBack]);
+  }
+
+  checkLength() {
+    if (this.categoryName.length > 50) {
+      this.lengthNameError = "Excede los 50 caracteres.";
+    } else {
+      this.lengthNameError = "";
+    }
+
+    if(this.urlBack.includes("brand") && this.categoryDescription.length > 120){
+      this.lengthDescriptionError = "Excede los 120 caracteres.";
+    } else if(this.categoryDescription.length > 90){
+      this.lengthDescriptionError = "Excede los 90 caracteres.";      
+    } else {
+      this.lengthDescriptionError = "";
+    }
   }
 }
